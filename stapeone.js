da023a508fc9e84ee73607352474702ce5a2c3b1
hakimcolor <hakimcolor777@gmail.com>
2025-08-26 10:30:31 +0600

    Add money section

---
+++ stapeone.js
@@ -0,0 +1,4 @@
+document.getElementById('Add-money').addEventListener('click', () => {
+  document.getElementById('stn').classList.toggle('hidden');
+  document.getElementById('Add-money').classList.toggle('border-red-500');
+});
